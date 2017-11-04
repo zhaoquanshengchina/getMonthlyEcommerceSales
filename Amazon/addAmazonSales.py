71086b1d9dfe0d70d4460977029d4d7798ba1c91
Zack <izaac.corbett@gmail.com>
2017-11-03 21:35:55 -0500

    Uses col/row to =sum, multiply sales with qty
    =sum sales column
    sales * qty rows
    =sum(sales*row) columns

--- Amazon/addAmazonSales.py
+++ Amazon/addAmazonSales.py
@@ -7,6 +7,7 @@ from datetime import datetime
 file = 'Amazon-Sold+Listings+Report+11-02-2017 - Copy.txt'#sys.argv[1]
 
 def AmazonSales(file): # Get only monthly sales for Amazon Marketplace sales list export
+	x = 1
 	with open(file, 'r') as amznTxt:
 		reader = csv.reader(amznTxt, delimiter='\t') # Amazon only exports as tab delimited txt
 		header = reader.next() # headers
@@ -26,10 +27,16 @@ def AmazonSales(file): # Get only monthly sales for Amazon Marketplace sales lis
 					purchaseDate = datetime.strptime(purchaseDate, '%Y-%m-%d') # Exclude the time, only year-month-day
 
 					if currentTime.strftime('%m') == purchaseDate.strftime('%m'): # If month of purchase date is current month
+						x += 1
+						print x
+
 						for each in index: # For each in row
 							amazonOutput.write("%s," % each) # Write entire rows contents to log file, comma delimited
+						amazonOutput.write("=sum(D%x*K%x)" % (x, x))
 						amazonOutput.write('\n') # new line escaped
 						print index
 					else: # Otherwise, from a previous month.
 						pass # pass.
+			amazonOutput.write(",,,=sum(D2:D%x),,,,,,,,=sum(L2:L%x)" % (x, x))
+			#amazonOutput.write("=sum(L2:L%x)" % x)
 AmazonSales(file)
