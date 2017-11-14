a879686114461db55b90907de9548e12e5402d05
BiTinerary <Zack@bitinerary.com>
2017-11-14 14:08:47 -0600

    added Total Sales log
    updated exe

--- parseBackupWithoutTotalsLog.py
+++ parseBackupWithoutTotalsLog.py
@@ -3,18 +3,31 @@ from datetime import datetime
 
 currentTime = datetime.now() # One global for current time/month. Used basically everywhere.
 
-def totalSalesLog(sales):
-	with open('totalSalesLog.txt', 'a+') as salesLog:
-		salesLog.write("%s \n" % sales)
-
 def replacePrint(output, index): # repetitive commands ran during each marketplace function call.
 	for each in index:
 		output.write('%s,' % each.replace(",", "")) # replace any rogue commas in product title, customer address, etc...
 	print index # print each row that matched monthly/SKU requirements
 	#Note: default spreadsheet header format of original marketplace spreadsheet is retained.
 
+def amazonIndexes(inputz, output, x):
+	for index in inputz:
+		if index[2][0:3] != "LUM":
+			pass
+
+		elif index[2][0:3] == "LUM":
+			purchaseDate = index[5].split(" ")[0] 
+			purchaseDate = datetime.strptime(purchaseDate, '%Y-%m-%d') 
+
+			if currentTime.strftime('%m') == purchaseDate.strftime('%m'): # Match numerical month string in spreadsheet, with current month. ie: 11, 12, etc...
+				replacePrint(output, index)
+				x += 1
+
+				output.write("=sum(D%s*K%s)" % (x, x)) # Amazon has line items, regardless of multi item orders. Multiply 'Quantity' with 'Sale price.'
+				output.write('\n') # new line escaped
+
+	output.write(",,,=SUM(D2:D%s),,,,,,,=SUM(K2:K%s),=SUM(L2:L%s)" % (x, x, x))
+
 def eBayIndexes(inputz, output, x): # Run on ebay Spreadsheet
-	total = 0
 	for index in inputz:
 		if index[31][0:3] != "LUM": # index 31 == 'Custom SKU'
 			pass # Is not a product we want total sales of
@@ -26,41 +39,12 @@ def eBayIndexes(inputz, output, x): # Run on ebay Spreadsheet
 				purchaseDate = datetime.strptime(purchaseDate, '%b-%d-%y') # make datetime instance using date format string from spreadsheet.
 				if currentTime.strftime('%b') == purchaseDate.strftime('%b'): # Match month abbreviation to current. ie: Nov, Dec, etc...
 					replacePrint(output, index) # See above
-
 					x += 1
-					total += float(index[20].replace("$", ""))
-					print total
 					output.write('\n')
-					
-	output.write(",,,,,,,,,,,,,,,=sum(P2:P%s),,,,,=sum(U2:U%s),,,,,,,,,,,,,,,,,,,,," % (x, x))
-	totalSalesLog("eBay: $%s" % total)
-
+	output.write(",,,,,,,,,,,,,,,=SUM(P2:P%s),,,,,=SUM(U2:U%s),,,,,,,,,,,,,,,,,,,,," % (x, x))
 	# Ugly code for last line in csv with spreadsheet functions in their respective columns.
 
-def amazonIndexes(inputz, output, x):
-	total = 0
-	for index in inputz:
-		if index[2][0:3] != "LUM":
-			pass
-
-		elif index[2][0:3] == "LUM":
-			purchaseDate = index[5].split(" ")[0] 
-			purchaseDate = datetime.strptime(purchaseDate, '%Y-%m-%d') 
-
-			if currentTime.strftime('%m') == purchaseDate.strftime('%m'): # Match numerical month string in spreadsheet, with current month. ie: 11, 12, etc...
-				replacePrint(output, index)
-
-				x += 1
-				total += float(index[3]) * float(index[10])
-				print total
-				output.write("=sum(D%s*K%s)" % (x, x)) # Amazon has line items, regardless of multi item orders. Multiply 'Quantity' with 'Sale price.'
-				output.write('\n') # new line escaped
-
-	output.write(",,,=sum(D2:D%s),,,,,,,,=sum(L2:L%s)" % (x, x))
-	totalSalesLog("Amazon: $%s" % total)
-
 def wallyIndexes(inputz, output, x):
-	total = 0
 	for index in inputz:
 		if index[20][0:3] != "LUM":
 			pass
@@ -71,14 +55,10 @@ def wallyIndexes(inputz, output, x):
 
 			if currentTime.strftime('%m') == purchaseDate.strftime('%m'):
 				replacePrint(output, index)
-				
 				x += 1
-				total += float(index[21])
-				print total
 				output.write('\n')
 
 	output.write(",,,,,,,,,,,,,,,,,,,=SUM(T2:T%s),,=SUM(V2:V%s),,,,,,,," % (x, x))
-	totalSalesLog("Wally: $%s" % total)
 	# Ugly code for last line in csv with spreadsheet functions in their respective columns.
 
 def parseBasedOn(storeName, inputz, output): # We quick check which store the spreadsheet belongs to
@@ -127,9 +107,6 @@ outputFolder = './output/' # hardcoded file for output, converted files.
 
 currentDirectory = next(os.walk('%s' % inputFolder))[2] # Get each spreadsheet filename
 print currentDirectory
-
-with open('totalSalesLog.txt', 'w+') as deleteExistingLog:
-	pass
 
 for eachFile in currentDirectory: #Run script on each file
 	makeFile('./%s%s' % (inputFolder, eachFile), outputFolder)
